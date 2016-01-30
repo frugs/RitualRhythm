42dcf3a8842fded969334f51767a0372a66e66b4
Damion Sheppard <d.l.sheppard@live.co.uk>
2016-01-30 16:19:59 +0000

    sound script

---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sound : MonoBehaviour {
+
+	public string music = "event:/Music/Beat";
+
+	public string enemyCount = "Enemy Count";
+
+	FMOD.Studio.EventInstance musicEv;
+	FMOD.Studio.ParameterInstance enemyCountPa;
+
+	// Use this for initialization
+	void Start () {
+
+		musicEv = FMODUnity.RuntimeManager.CreateInstance (music);
+
+		musicEv.getParameter (enemyCount, out enemyCountPa);
+
+		// timelinePos = musicEv.getTimelinePosition;
+
+		musicEv.start ();
+
+	}
+
+	void enemyCountChange()
+	{
+		enemyCountPa.setValue ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
