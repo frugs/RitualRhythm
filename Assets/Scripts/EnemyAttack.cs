58c0a0a1d71efd61626299000170c4f725d1e9cf
James Cox <james@jamesac.co.uk>
2016-01-30 18:36:49 +0000

    Check for beat. Start of enemy action on beat

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAttack : MonoBehaviour {
+
+	public Sound soundManager;
+	SpriteRenderer sprite;
+
+	// Use this for initialization
+	void Start () {
+		 sprite = gameObject.GetComponent (SpriteRenderer);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int offset;
+		if (soundManager.isOnBeat (offset)) {
+			sprite.color.r = 1.0;
+		} else {
+			sprite.color.r = 0.0;
+		}
+	}
+}
