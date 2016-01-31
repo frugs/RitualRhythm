aa26edd0473d7d3b2a190946fa2e87e9074de7f8
Hugo Wainwright <hwainwright@gmx.com>
2016-01-31 04:19:02 +0000

    basic ai

--- Assets/Scripts/Actor/Enemy/EnemyAIController.cs
+++ Assets/Scripts/Actor/Enemy/EnemyAIController.cs
@@ -1,8 +1,67 @@
-﻿namespace RitualRhythm.Actor.Enemy {
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace RitualRhythm.Actor.Enemy {
     public class EnemyAiController {
 
-        public void Update() {
-            
+        private enum AiType {
+            Attack,
+            Idle,
+            WanderAround
+        }
+
+        private readonly ActorModel _actorModel;
+        private readonly float _attackRange;
+        private readonly float _velocity;
+
+        private AiType _aiType;
+        private Vector2 _wanderDirection;
+        private double _decisionTimer;
+
+        public EnemyAiController(ActorModel actorModel, float attackRange, float velocity) {
+            this._actorModel = actorModel;
+            _attackRange = attackRange;
+            _velocity = velocity;
+
+            PickAiType();
+            ResetDecisionTimer();
+        }
+
+        private void PickAiType() {
+            var values = Enum.GetValues(typeof(AiType));
+            _aiType = (AiType)values.GetValue(Random.Range(0, values.Length));
+            _wanderDirection = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+
+        }
+
+        public void Update(float deltaTime, Vector2 playerPosition) {
+            _decisionTimer -= deltaTime;
+            if (_decisionTimer < 0f) {
+                ResetDecisionTimer();
+                PickAiType();
+            }
+
+            var playerDisplacement = playerPosition - _actorModel.Position;
+            var playerDirection = playerDisplacement.normalized;
+            _actorModel.LookTowards(playerDirection.x > 0 ? Vector2.right : Vector2.left);
+
+            if (_aiType == AiType.Attack) {
+                if (playerDisplacement.magnitude >_attackRange) {
+                    var moveBy = playerDirection * _velocity * deltaTime;
+                    _actorModel.MoveBy(moveBy);
+                } else if (Random.value > 0.7f) {
+                    _actorModel.Attack();
+                }
+            }
+
+            if (_aiType == AiType.WanderAround) {
+                _actorModel.MoveBy(_wanderDirection * _velocity * deltaTime);
+            }
+        }
+
+        private void ResetDecisionTimer() {
+            _decisionTimer = Random.Range(3f, 6f);
         }
     }
 }
--- Assets/Scripts/Actor/Enemy/EnemyBehaviour.cs
+++ Assets/Scripts/Actor/Enemy/EnemyBehaviour.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using RitualRhythm;
 using RitualRhythm.Actor;
+using RitualRhythm.Actor.Player;
 using UnityEngine;
 
 namespace RitualRhythm.Actor.Enemy {
-    public class EnemyBehaviour : MonoBehaviour {
+    public class EnemyBehaviour : ActorBehaviour {
         private const float HurtAnimationLength = 0.25f;
+
+        private EnemyAiController AiController;
 
         private SpriteRenderer _spriteRenderer;
 		public Sound soundManager;
@@ -16,18 +19,27 @@ namespace RitualRhythm.Actor.Enemy {
         private IEnumerator _hurtAnimRoutine = EnumeratorUtils.EmptyEnumerator();
         private Color _originalColor;
 
-        public void Start () {
+        public PlayerBehaviour PlayerBehaviour;
+
+        public override void Start () {
             _spriteRenderer = GetComponent<SpriteRenderer>();
             _originalColor = _spriteRenderer.color;
+            base.Start();
+
+            AiController = new EnemyAiController(ActorModel, 1f, 3f);
         }
 	
-        public void Update () {
+        public override void Update () {
             if (_hurt) {
                 _hurtAnimRoutine = PlayHurtAnimation();
 				soundManager.playPain();
                 _hurt = false;
             }
             _hurtAnimRoutine.MoveNext();
+
+            AiController.Update(Time.deltaTime, PlayerBehaviour.ActorModel.Position);
+
+            base.Update();
         }
 
         public void OnTriggerEnter(Collider col) {
