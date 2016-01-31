090706812e19da04d68ce860e832e0de111c8e30
Hugo Wainwright <hwainwright@gmx.com>
2016-01-31 10:27:39 +0000

    Boss music

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -4,12 +4,18 @@ namespace RitualRhythm {
     public class CameraBehaviour : MonoBehaviour {
 
         public GameObject FollowTarget;
+        public GameObject Boss;
+        public Sound Sound;
 
         private float _minX;
 
         public void Update () {
             _minX = Mathf.Max(_minX, FollowTarget.transform.position.x);
             transform.position = new Vector3(_minX, transform.position.y, transform.position.z);
+
+            if (Mathf.Abs(Boss.transform.position.x - transform.position.x) < 1f) {
+                Sound.setState(2);
+            }
         }
     }
 }
